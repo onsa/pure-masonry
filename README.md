c916d2280003f0dd23f6fc7aa6161afc9c5febcb
Janos Nemeth <janos.nemeth@caliberi.com>
2016-12-15 13:17:31 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Pure Masonry
+
+Masonry DOM effect with pure JavaScript
+
+**Installation**
+
+```bash
+npm install --save-dev pure-masonry
+```
+
+**Usage**
+
+**1.**
+Add pureMasonry.min.css and pureMasonry.min.js to you project code.
+```html
+<head>
+	...
+	<link rel="stylesheet" href="node_modules/pure-masonry/src/pureMasonry.min.css">
+	...
+</head>
+<body>
+	...
+	<script type="text/javascript" src="node_modules/pure-masonry/src/pureMasonry.min.js"></script>
+</body>
+```
+**2.**
+Add a container wall to your html body with bricks inside:
+```html
+<div ID="masonry-wall">
+	<div class="brick">
+		...
+	</div>
+	(more bricks... )
+</div>
+```
+**3.**
+Initialise Pure Masonry with your preferred brick width, gap size between columns and gap size under each brick (all values in px):
+```javascript
+mason.grabTrowel(brickWidth, horizontalGutter, verticalGutter);
+```
+**4.**
+You can programmatically suspend Pure Masonry by setting the variable 'mason.options.underConstruction to false'. This prevents the rearrangement of bricks on browser resize.
---
+++ src/pureMasonry.js
@@ -0,0 +1,91 @@
+//	get container element
+var brickContainer = document.getElementById('masonry-wall');
+
+var build = function(options) {
+
+	//	get each brick
+	var bricks = [];
+	for (var brickIndex = 0; brickIndex < brickContainer.children.length; brickIndex++) {
+		var classNames = brickContainer.children[brickIndex].className.split(' ');
+		if (classNames.indexOf('brick') > -1) {
+			brickContainer.children[brickIndex].style.width = options.brickWidth + 'px';
+			bricks.push(brickContainer.children[brickIndex]);
+		}
+	}
+
+	var grossWidth = options.brickWidth + options.horizontalGutter;
+
+	//	calculate the number of bricks in each row
+	var bricksPerRow = Math.floor(parseInt(brickContainer.clientWidth) / grossWidth);
+
+	//	initialise array to keep track of column height
+	var columnHeight = Array(bricksPerRow).fill(0);
+
+	//	populate first row starting with first (0th) brick
+	var brickIndex = 0;
+	for (var column = 0; column < bricksPerRow; column++) {
+		if (brickIndex < bricks.length) {
+			//	set coordinates for brick
+			bricks[brickIndex].style.left = column * grossWidth + 'px';
+			bricks[brickIndex].style.top = '0px';
+			//	update the height of the column just appended
+			columnHeight[column] = bricks[brickIndex].offsetHeight;
+			brickIndex++;
+		}
+	}
+
+	//	place remaining bricks
+	while (brickIndex < bricks.length) {
+		//	get shortest column
+		var minColumnValue = Math.min.apply(Math, columnHeight);
+		var minColumnKey;
+		for (var column = 0; column < bricksPerRow; column++) {
+			//	find the key for the minimum value
+			if (columnHeight[column] === minColumnValue) {
+				minColumnKey = column;
+				//	use the leftmost in case several columns have the same height
+				break;
+			}
+		}
+		//	set coordinates for brick
+		bricks[brickIndex].style.left = minColumnKey * grossWidth + 'px';
+		bricks[brickIndex].style.top = columnHeight[minColumnKey] + options.verticalGutter + 'px';
+		//	update the height of the column just appended
+		columnHeight[minColumnKey] += bricks[brickIndex].offsetHeight + options.verticalGutter;
+		brickIndex++;
+	}
+}
+
+//	if browser is resized
+window.onresize = function() {
+	//	if masonry is not disabled
+	if (mason.options.underConstruction) {
+		var widthBefore = brickContainer.clientWidth;			//	get width before resizing
+		//	if already called within last second, reset timer
+		if (waitingForResize) {
+			clearTimeout(waitingForResize);
+		}
+		var waitingForResize = setTimeout(function() {
+			//	if container width has changed in the last second
+			if (widthBefore !== brickContainer.clientWidth) {
+				build(mason.options);
+			}
+		}, 1000);
+    }
+};
+
+//	initialise main object
+var mason = {
+	options: {},
+
+	grabTrowel: function(brickWidth, horizontalGutter, verticalGutter) {
+		this.options = {
+			brickWidth:	brickWidth,
+			horizontalGutter:	horizontalGutter,
+			verticalGutter:	verticalGutter,
+			underConstruction: true
+		};
+
+		build(this.options);
+	}
+};
